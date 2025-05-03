ec9a18b4857cf6a72f830da234c92fd7d834ca85
HelChris <111705707+HelChris@users.noreply.github.com>
2025-05-03 19:05:39 +0200

    test husky

--- src/components/testing.js
+++ src/components/testing.js
@@ -7,11 +7,21 @@ function createHeader() {
   const h1 = document.createElement("h1");
   h1.textContent = "Hi, is this working?";
   header.appendChild(h1);
-  const h1 = document.createElement("h2");
+  const h2 = document.createElement("h2");
   h2.textContent = "This test is going so well?";
   h1.appendChild(h2);
-           return header;
+  return header;
 }
 
+function createHeader() {
+  const header = document.querySelector("header");
+  const h1 = document.createElement("h1");
+  h1.textContent = "Hi, is this working?";
+  header.appendChild(h1);
+  const h2 = document.createElement("h2");
+  h2.textContent = "This test is going so well?";
+  h1.appendChild(h2);
+  return header;
+}
 // Example usage:
 document.body.prepend(createHeader());
