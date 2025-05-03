2662364a03037e3409d40c65147e17948de2bdda
HelChris <111705707+HelChris@users.noreply.github.com>
2025-05-03 17:29:20 +0200

    test husky

--- src/components/testing.js
+++ src/components/testing.js
@@ -8,8 +8,8 @@ function createHeader() {
   h1.textContent = "Hi, is this working?";
   header.appendChild(h1);
   const h2 = document.createElement("h2");
-  h2.textContent = "This test is going so well!";
-  h1.appendChild(h2);
+  h2.  textContent = "This test is going so well!";
+  h1.appendChild(h3);
   return header;
 }
 
