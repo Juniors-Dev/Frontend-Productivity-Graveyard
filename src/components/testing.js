db10eac34d219e1d4330ada532ae854ccdf66714
HelChris <111705707+HelChris@users.noreply.github.com>
2025-05-03 16:49:33 +0200

    test if husky is working

--- src/components/testing.js
+++ src/components/testing.js
@@ -1,2 +1,14 @@
 //husky test
 // testing testing one, two
+// testing again. yay
+
+function createHeader() {
+   const header = document.querySelector("header");
+   const h1 = document.createElement("h1");
+  h1.textContent = "Hello, is this working now?";
+  header.appendChild(h1);
+  return header ;
+}
+
+// Example usage:
+document.body.prepend(createHeader());
