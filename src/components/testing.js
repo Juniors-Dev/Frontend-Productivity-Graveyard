430a7a841678e02ce1abc99ee411d85690b2038a
HelChris <111705707+HelChris@users.noreply.github.com>
2025-05-03 17:07:20 +0200

    update pre-commit to husky v10

--- src/components/testing.js
+++ src/components/testing.js
@@ -3,10 +3,10 @@
 // testing again. yay
 
 function createHeader() {
-   const header = document.querySelector("header");
-   const h1 = document.createElement("h1");
-  h1.textContent = "Hello, is this working now?";
-  header.appendChild(h1);
+   const      header = document.querySelector("header");
+   const h 1 = document.createElement("h1");
+  h1.textContent = "Hello, is this working?";
+            header.appendChild(h1);
   return header ;
 }
 
