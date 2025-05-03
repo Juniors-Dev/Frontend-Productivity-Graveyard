97fba003bc6ed9a1b1518f3b025bf34d00eb78cc
HelChris <111705707+HelChris@users.noreply.github.com>
2025-05-03 21:23:10 +0200

    husky test no.200

--- src/components/testing.js
+++ src/components/testing.js
@@ -1,17 +1,1 @@
-//husky test
-// testing testing one, two
-// testing again. yay
-
-function createHeader() {
-  const header = document.querySelector("header");
-  const h1 = document.createElement("h1");
-  h1.textContent = "Hi, is this working?";
-  header.appendChild(h1);
-  const h2 = document.createElement("h2");
-  h22.textContent = "This teeeeest is going so well?";
-  h1.appendChild(h2);
-  return header;
-}
-}
-// Example usage:
-document.body.prepend(createHeader());
+//husky test n.200
