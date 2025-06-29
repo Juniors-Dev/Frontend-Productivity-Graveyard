aea4efeb2ae6d7084e138373040229f6431234dc
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-06-29 23:42:36 +0200

    profile page and card changes

--- src/components/features/profileProjectCard/profileProjectCard.js
+++ src/components/features/profileProjectCard/profileProjectCard.js
@@ -1,7 +1,7 @@
 import { baseUrl } from "../../../utlis/constants.js";
 import { calculateLifespan, formatUnit } from "../../../utlis/dateHandlers.js";
 
-export function renderProfileProjectCard(project) {
+export function profileProjectCard(project) {
   // Create a new project card container
   const projectCard = document.createElement("div");
   projectCard.classList.add("funeral-card");
@@ -30,18 +30,17 @@ export function renderProfileProjectCard(project) {
 
   // Add years and likes
   const lifespan = calculateLifespan(project);
-  console.log(lifespan);
   const { years, months, days } = lifespan.breakdown;
 
   const yearsLikes = document.createElement("div");
   yearsLikes.classList.add("yearslikes");
   const yearsCont = document.createElement("div");
   yearsCont.innerHTML = `<span class="years">Lifespan: </span><span>${formatUnit(years, "year", "years")}${formatUnit(months, "month", "months")}${formatUnit(days, "day", "days")}</span>`;
-  const likes = document.createElement("div");
+  const likes = document.createElement("button");
   likes.classList.add("btn-likes");
   likes.innerHTML = `
       <img src="./src/assets/img/thumb.png" alt="Like Icon" class="like-icon" width="25px" height="25px" />
-      <span>${project.likes}</span>
+      <span>${project.upvoteCount}</span>
     `;
   yearsLikes.appendChild(yearsCont);
   yearsLikes.appendChild(likes);
@@ -49,8 +48,76 @@ export function renderProfileProjectCard(project) {
   // Assemble the project card
   projectCard.appendChild(projectImageContainer);
   projectCard.appendChild(projectInfo);
-  projectCard.appendChild(yearsLikes);
+  projectInfo.appendChild(yearsLikes);
 
   // Append the project card to the recent funerals container
   return projectCard;
+}
+
+export function profileProjectCardSkeleton() {
+  return `
+    <div class="funeral-card skeleton-card">
+      <div class="funeral-image">
+        <div class="skeleton skeleton-img"></div>
+      </div>
+      <div class="project-info">
+        <div class="skeleton skeleton-title"></div>
+        <div class="skeleton skeleton-text"></div>
+        <div class="yearslikes">
+          <div>
+            <span class="years skeleton skeleton-line" style="display: inline-block; width: 100px;"></span>
+            <div class="skeleton skeleton-line" style="width: 140px;"></div>
+          </div>
+          <button class="btn-likes">
+            <div class="skeleton skeleton-icon"></div>
+            <div class="skeleton skeleton-tiny"></div>
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+  // return `<div class="funeral-card"><div class="funeral-image"><img class="skeleton skeleton-img" src="http://localhost:3000/images/usb_coffin.png" alt="USB Coffin Icon" width="120" height="120"></div><div class="project-info"><h3>Productivity Graveyard</h3><p>A humorous app to memorialize abandoned dev projects.</p></div><div class="yearslikes"><div><span class="years">Lifespan: </span><span>3 months 14 days </span></div><div class="btn-likes">
+  //     <img src="./src/assets/img/thumb.png" alt="Like Icon" class="like-icon" width="25px" height="25px">
+  //     <span>undefined</span>
+  //   </div></div></div>`;
+  // return `<div class="project-card skeleton-card skeleton-container">
+  //             <div class="card-left">
+  //               <div class="skeleton skeleton-img"></div>
+  //               <div class="skeleton skeleton-title"></div>
+  //               <div class="timeauthor">
+  //                 <div class="skeleton skeleton-small"></div>
+  //                 <div class="skeleton skeleton-small"></div>
+  //               </div>
+  //             </div>
+  //             <div class="card-right">
+  //               <div class="project-description">
+  //                 <div class="skeleton skeleton-title"></div>
+  //                 <div class="skeleton skeleton-text"></div>
+  //               </div>
+  //               <div
+  //                 style="
+  //                   display: flex;
+  //                   flex-direction: row;
+  //                   align-items: center;
+  //                   gap: 20px;
+  //                 "
+  //               >
+  //                 <div class="project-info">
+  //                   <div class="skeleton skeleton-line"></div>
+  //                   <div class="skeleton skeleton-line"></div>
+  //                   <div class="skeleton skeleton-line"></div>
+  //                 </div>
+  //                 <div class="btn-likes">
+  //                   <img
+  //                     src="./src/assets/img/thumb.png"
+  //                     alt="Like Icon"
+  //                     class="like-icon"
+  //                     width="25px"
+  //                     height="25px"
+  //                   />
+  //                   <div class="skeleton skeleton-tiny"></div>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>`;
 }
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,20 +1,56 @@
-import { renderProfileProjectCard } from "../components/features/profileProjectCard/profileProjectCard.js";
+import {
+  profileProjectCard,
+  profileProjectCardSkeleton,
+} from "../components/features/profileProjectCard/profileProjectCard.js";
 import { renderProfile } from "../components/features/profile/profile.js";
 import { api } from "../main.js";
+import { pagination } from "../components/features/pagination/pagination.js";
+
+const projectsContainer = document.querySelector(".users-projects");
+function showSkeletons(count) {
+  projectsContainer.innerHTML = "";
+  for (let i = 0; i < count; i++) {
+    projectsContainer.innerHTML += profileProjectCardSkeleton();
+  }
+}
 
 let user;
+//check if user id is included
 const urlParams = new URLSearchParams(window.location.search);
 let userId = urlParams.get("id") || null;
-
+showSkeletons(10);
 if (userId) {
   user = await api.getUserById(userId);
 } else {
   user = await api.getCurrentUser();
 }
 
+//render users details
 renderProfile(user.data);
 
-const projectsContainer = document.querySelector(".users-projects");
+//render projects
+
+projectsContainer.innerHTML = "";
 user.data.projects.data.forEach((project) => {
-  projectsContainer.append(renderProfileProjectCard(project));
+  projectsContainer.append(profileProjectCard(project));
 });
+
+console.log(user.data.projects);
+
+const { hasNext, limit, total } = user.data.projects.meta;
+let currentPage = 1;
+let totalPages = Math.ceil(total / limit);
+const paginationContainer = document.querySelector(".pagination");
+if (hasNext) {
+  pagination({
+    current: currentPage,
+    total: totalPages,
+    container: paginationContainer,
+    onPageChange: (newPage) => {
+      currentPage = newPage;
+      //fetchAndRenderProjects(newPage);
+    },
+  });
+}
+
+// async function fetchAndRenderProjects(pageNumber) {}
