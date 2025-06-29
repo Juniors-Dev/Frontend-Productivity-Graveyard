19c7db2cd06395c020a997957159dacb6efe036d
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-06-30 00:59:31 +0200

    change url back

--- src/utlis/constants.js
+++ src/utlis/constants.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://backend-productivity-graveyard.onrender.com";
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://backend-productivity-graveyard.onrender.com";
+// export const baseUrl = "http://localhost:3000";
