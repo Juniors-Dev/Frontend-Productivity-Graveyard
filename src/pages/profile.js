53057fb8e50c889cedf8ea326bd0193d5e64399f
Anclagen <91462616+Anclagen@users.noreply.github.com>
2025-06-30 00:57:48 +0200

    basic error handling

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -13,44 +13,93 @@ function showSkeletons(count) {
     projectsContainer.innerHTML += profileProjectCardSkeleton();
   }
 }
-
 let user;
-//check if user id is included
-const urlParams = new URLSearchParams(window.location.search);
-let userId = urlParams.get("id") || null;
-showSkeletons(10);
-if (userId) {
-  user = await api.getUserById(userId);
-} else {
-  user = await api.getCurrentUser();
-}
-
-//render users details
-renderProfile(user.data);
-
-//render projects
-
-projectsContainer.innerHTML = "";
-user.data.projects.data.forEach((project) => {
-  projectsContainer.append(profileProjectCard(project));
-});
-
-console.log(user.data.projects);
-
-const { hasNext, limit, total } = user.data.projects.meta;
 let currentPage = 1;
-let totalPages = Math.ceil(total / limit);
+let limit = 10;
 const paginationContainer = document.querySelector(".pagination");
-if (hasNext) {
-  pagination({
-    current: currentPage,
-    total: totalPages,
-    container: paginationContainer,
-    onPageChange: (newPage) => {
-      currentPage = newPage;
-      //fetchAndRenderProjects(newPage);
-    },
+try {
+  //check if user id is included
+  const urlParams = new URLSearchParams(window.location.search);
+  let userId = urlParams.get("id") || null;
+  showSkeletons(10);
+  if (userId) {
+    user = await api.getUserById(userId);
+  } else {
+    user = await api.getCurrentUser();
+  }
+
+  if (!user.success) {
+    throw new Error("Failed to fetch user");
+  }
+
+  //render users details
+  renderProfile(user.data);
+
+  //render projects
+
+  projectsContainer.innerHTML = "";
+  user.data.projects.data.forEach((project) => {
+    projectsContainer.append(profileProjectCard(project));
   });
+
+  console.log(user.data.projects);
+
+  const { hasNext, total } = user.data.projects.meta;
+
+  let totalPages = Math.ceil(total / limit);
+
+  if (hasNext) {
+    pagination({
+      current: currentPage,
+      total: totalPages,
+      container: paginationContainer,
+      onPageChange: (newPage) => {
+        currentPage = newPage;
+        fetchAndRenderProjects(newPage);
+      },
+    });
+  }
+} catch (err) {
+  console.log(err);
+  projectsContainer.innerHTML =
+    "<p>An error occurred fetching the user and their projects, please refresh the page.</p>";
 }
 
-// async function fetchAndRenderProjects(pageNumber) {}
+async function fetchAndRenderProjects(page) {
+  try {
+    showSkeletons(10);
+    const offset = (page - 1) * limit;
+    const projects = await api.getAllProjects({
+      userId: user.data.id,
+      limit,
+      offset,
+    });
+    if (!projects.success) {
+      projectsContainer.innerHTML =
+        "<p>An error occurred fetching projects, please refresh the page.</p>";
+      return;
+    }
+    projectsContainer.innerHTML = "";
+    if (projects.data.length > 0) {
+      projects.data.forEach((project) => {
+        projectsContainer.append(profileProjectCard(project));
+      });
+      pagination({
+        current: currentPage,
+        total: Math.ceil(projects.meta.total / limit),
+        container: paginationContainer,
+        onPageChange: (newPage) => {
+          currentPage = newPage;
+          fetchAndRenderProjects(newPage);
+        },
+      });
+    } else {
+      projectsContainer.innerHTML =
+        "<p>No projects found, bury a regret today!.</p>";
+    }
+  } catch (err) {
+    console.log(err);
+    projectsContainer.innerHTML =
+      "<p>An error occurred fetching projects, please refresh the page.</p>";
+  }
+}
